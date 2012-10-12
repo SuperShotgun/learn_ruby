05ad9121e132287ba02280b7f011e8fb36299320
SuperShotgun <Protectr20@gmail.com>
2012-10-12 15:49:29 +0400

    Update 02_calculator/calculator.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,9 +1,9 @@
-def add(z1,z2)
-  return z1+z2
+def add(z1, z2)
+  return z1 + z2
 end
 
-def subtract(a,b)
-return a-b
+def subtract(a, b)
+return a - b
 end
 
 
@@ -11,11 +11,11 @@ def sum(arr)
 arr.inject(0){ |result, elem| result = result + elem }
 end
 
-def power(a,b)
+def power(a, b)
 return a**b
 end  
 
-def multiply(a,c,d)
+def multiply(a, c, d)
 return a*c*d
 end  
 
