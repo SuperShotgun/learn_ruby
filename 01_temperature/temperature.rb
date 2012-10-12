475ffdc6f492b46d4a732d713c79698678b5afd3
SuperShotgun <Protectr20@gmail.com>
2012-10-12 19:26:34 +0400

    Update 01_temperature/temperature.rb

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,30 +1,8 @@
-
-
-f=32.0..9999.0
-f=0.0..9999.0
-def ftoc(f)
-case f
-when 32 
-	return 0
-when 212
-	return 100
-when 98.6
-	return 37
-when 68
-	return 20
+def ftoc(farengeit)
+  (farengeit-32.0) * (5.0/9.0)
 end
 
+def ctof(celsium)
+  celsium * (9.0/5.0) + 32
 end
 
-def ctof(c)
-case c
-when 0
-	return 32
-when 100
-	return 212
-when 20
-	return 68
-when 37.0
-	return 98.6
-end
-end	
