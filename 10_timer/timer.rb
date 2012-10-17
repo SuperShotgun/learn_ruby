7cc40fbe1f167287de883fb366db1ff048b80f93
SuperShotgun <Protectr20@gmail.com>
2012-10-17 16:34:27 +0400

    Update 10_timer/timer.rb

--- 10_timer/timer.rb
+++ 10_timer/timer.rb
@@ -6,9 +6,9 @@ class Timer
   end
 
   def time_string
-    hours = @seconds/3600
-    remainder = @seconds%3600
-    sprintf("%02d:%02d:%02d", hours, remainder/60, remainder%60)
+    hours = @seconds / 3600
+    remainder = @seconds % 3600
+    sprintf("%02d:%02d:%02d", hours, remainder / 60, remainder % 60)
   end
   
 end
