3e6a7ab72eb9f8cd45090bb1fb809f9e94963232
SuperShotgun <Protectr20@gmail.com>
2013-04-16 16:24:35 +0400

    Update book.rb

--- 09_book_titles/book.rb
+++ 09_book_titles/book.rb
@@ -1,14 +1,12 @@
 class Book
   attr_reader :title
-
   def title=(new_title)
     words = new_title.split
-    little_words = %w{a an and the in of}
+    little_words = ["a", "an", "and", "the", "in", "of"]
     words = [words[0].capitalize] +
       words[1..-1].map do |word|
          little_words.include?(word)? word : word.capitalize
       end
     @title = words.join(" ")
   end
-
 end
